a0a4f8e3247bc70d1112799a4ef23ddbfaedd5d7
Raihan Khalil Abdillah <tatuhey@gmail.com>
2023-05-11 00:00:29 +0800

    6.5 still incomplete. 6.11 is okayish. 6.12 6.13 are OK. 6.14 6.15 still incomplete. 6.16 COMMENTED 6.17 is OK.
    Need to do buttons separately.

--- ServiceApplication-FormsApp/Form1.Designer.cs
+++ ServiceApplication-FormsApp/Form1.Designer.cs
@@ -33,7 +33,17 @@
             this.tbProblem = new System.Windows.Forms.TextBox();
             this.tbCost = new System.Windows.Forms.TextBox();
             this.lvRegular = new System.Windows.Forms.ListView();
+            this.colHeadName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadModel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadProblem = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadTag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadCost = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lvExpress = new System.Windows.Forms.ListView();
+            this.colHeadName1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadModel1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadProblem1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadTag1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeadCost1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lbFinished = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -49,6 +59,8 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.btnPay = new System.Windows.Forms.Button();
+            this.btnRegular = new System.Windows.Forms.Button();
+            this.btnExpress = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numTag)).BeginInit();
             this.gbPriority.SuspendLayout();
             this.SuspendLayout();
@@ -75,12 +87,12 @@
             this.tbProblem.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tbProblem.Multiline = true;
             this.tbProblem.Name = "tbProblem";
-            this.tbProblem.Size = new System.Drawing.Size(206, 135);
+            this.tbProblem.Size = new System.Drawing.Size(206, 181);
             this.tbProblem.TabIndex = 2;
             // 
             // tbCost
             // 
-            this.tbCost.Location = new System.Drawing.Point(55, 367);
+            this.tbCost.Location = new System.Drawing.Point(55, 413);
             this.tbCost.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tbCost.Name = "tbCost";
             this.tbCost.Size = new System.Drawing.Size(206, 20);
@@ -89,31 +101,97 @@
             // 
             // lvRegular
             // 
+            this.lvRegular.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colHeadName,
+            this.colHeadModel,
+            this.colHeadProblem,
+            this.colHeadTag,
+            this.colHeadCost});
             this.lvRegular.HideSelection = false;
-            this.lvRegular.Location = new System.Drawing.Point(317, 63);
+            this.lvRegular.Location = new System.Drawing.Point(317, 62);
             this.lvRegular.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.lvRegular.Name = "lvRegular";
             this.lvRegular.Size = new System.Drawing.Size(380, 160);
             this.lvRegular.TabIndex = 4;
             this.lvRegular.UseCompatibleStateImageBehavior = false;
+            this.lvRegular.View = System.Windows.Forms.View.Details;
+            this.lvRegular.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lvRegular_MouseClick);
+            // 
+            // colHeadName
+            // 
+            this.colHeadName.Text = "Client Name";
+            this.colHeadName.Width = 86;
+            // 
+            // colHeadModel
+            // 
+            this.colHeadModel.Text = "Drone Model";
+            this.colHeadModel.Width = 87;
+            // 
+            // colHeadProblem
+            // 
+            this.colHeadProblem.Text = "Service Problem";
+            this.colHeadProblem.Width = 108;
+            // 
+            // colHeadTag
+            // 
+            this.colHeadTag.Text = "Service Tag";
+            this.colHeadTag.Width = 98;
+            // 
+            // colHeadCost
+            // 
+            this.colHeadCost.Text = "Service Cost";
             // 
             // lvExpress
             // 
+            this.lvExpress.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colHeadName1,
+            this.colHeadModel1,
+            this.colHeadProblem1,
+            this.colHeadTag1,
+            this.colHeadCost1});
             this.lvExpress.HideSelection = false;
-            this.lvExpress.Location = new System.Drawing.Point(317, 256);
+            this.lvExpress.Location = new System.Drawing.Point(317, 272);
             this.lvExpress.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.lvExpress.Name = "lvExpress";
             this.lvExpress.Size = new System.Drawing.Size(380, 160);
             this.lvExpress.TabIndex = 5;
             this.lvExpress.UseCompatibleStateImageBehavior = false;
+            this.lvExpress.View = System.Windows.Forms.View.Details;
+            this.lvExpress.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lvExpress_MouseClick);
+            // 
+            // colHeadName1
+            // 
+            this.colHeadName1.Text = "Client Name";
+            this.colHeadName1.Width = 92;
+            // 
+            // colHeadModel1
+            // 
+            this.colHeadModel1.Text = "Drone Model";
+            this.colHeadModel1.Width = 87;
+            // 
+            // colHeadProblem1
+            // 
+            this.colHeadProblem1.Text = "Service Problem";
+            this.colHeadProblem1.Width = 112;
+            // 
+            // colHeadTag1
+            // 
+            this.colHeadTag1.Text = "Service Tag";
+            this.colHeadTag1.Width = 81;
+            // 
+            // colHeadCost1
+            // 
+            this.colHeadCost1.Text = "Service Cost";
+            this.colHeadCost1.Width = 87;
             // 
             // lbFinished
             // 
+            this.lbFinished.ColumnWidth = 1;
             this.lbFinished.FormattingEnabled = true;
-            this.lbFinished.Location = new System.Drawing.Point(747, 63);
+            this.lbFinished.Location = new System.Drawing.Point(751, 62);
             this.lbFinished.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.lbFinished.Name = "lbFinished";
-            this.lbFinished.Size = new System.Drawing.Size(146, 290);
+            this.lbFinished.Size = new System.Drawing.Size(146, 329);
             this.lbFinished.TabIndex = 6;
             // 
             // label1
@@ -151,7 +229,7 @@
             // 
             // numTag
             // 
-            this.numTag.Location = new System.Drawing.Point(55, 300);
+            this.numTag.Location = new System.Drawing.Point(55, 346);
             this.numTag.Name = "numTag";
             this.numTag.Size = new System.Drawing.Size(52, 20);
             this.numTag.TabIndex = 10;
@@ -160,7 +238,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(52, 282);
+            this.label4.Location = new System.Drawing.Point(52, 328);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 15);
@@ -171,7 +249,7 @@
             // 
             this.gbPriority.Controls.Add(this.rbExpress);
             this.gbPriority.Controls.Add(this.rbRegular);
-            this.gbPriority.Location = new System.Drawing.Point(141, 285);
+            this.gbPriority.Location = new System.Drawing.Point(141, 331);
             this.gbPriority.Name = "gbPriority";
             this.gbPriority.Size = new System.Drawing.Size(121, 61);
             this.gbPriority.TabIndex = 13;
@@ -181,7 +259,7 @@
             // rbExpress
             // 
             this.rbExpress.AutoSize = true;
-            this.rbExpress.Location = new System.Drawing.Point(6, 39);
+            this.rbExpress.Location = new System.Drawing.Point(6, 38);
             this.rbExpress.Name = "rbExpress";
             this.rbExpress.Size = new System.Drawing.Size(69, 17);
             this.rbExpress.TabIndex = 1;
@@ -204,7 +282,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(52, 349);
+            this.label5.Location = new System.Drawing.Point(52, 395);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(86, 15);
@@ -213,12 +291,13 @@
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(55, 393);
+            this.btnAdd.Location = new System.Drawing.Point(55, 439);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
             this.btnAdd.TabIndex = 15;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // label6
             // 
@@ -235,7 +314,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(314, 238);
+            this.label7.Location = new System.Drawing.Point(314, 254);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(58, 15);
@@ -255,18 +334,40 @@
             // 
             // btnPay
             // 
-            this.btnPay.Location = new System.Drawing.Point(747, 359);
+            this.btnPay.Location = new System.Drawing.Point(751, 397);
             this.btnPay.Name = "btnPay";
             this.btnPay.Size = new System.Drawing.Size(75, 57);
             this.btnPay.TabIndex = 19;
             this.btnPay.Text = "Pay";
             this.btnPay.UseVisualStyleBackColor = true;
             // 
+            // btnRegular
+            // 
+            this.btnRegular.Location = new System.Drawing.Point(317, 228);
+            this.btnRegular.Name = "btnRegular";
+            this.btnRegular.Size = new System.Drawing.Size(75, 23);
+            this.btnRegular.TabIndex = 20;
+            this.btnRegular.Text = "Remove";
+            this.btnRegular.UseVisualStyleBackColor = true;
+            this.btnRegular.Click += new System.EventHandler(this.btnRegular_Click);
+            // 
+            // btnExpress
+            // 
+            this.btnExpress.Location = new System.Drawing.Point(317, 438);
+            this.btnExpress.Name = "btnExpress";
+            this.btnExpress.Size = new System.Drawing.Size(75, 23);
+            this.btnExpress.TabIndex = 21;
+            this.btnExpress.Text = "Remove";
+            this.btnExpress.UseVisualStyleBackColor = true;
+            this.btnExpress.Click += new System.EventHandler(this.btnExpress_Click);
+            // 
             // ServiceApplicationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(958, 466);
+            this.ClientSize = new System.Drawing.Size(958, 522);
+            this.Controls.Add(this.btnExpress);
+            this.Controls.Add(this.btnRegular);
             this.Controls.Add(this.btnPay);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -321,6 +422,18 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button btnPay;
+        private System.Windows.Forms.Button btnRegular;
+        private System.Windows.Forms.Button btnExpress;
+        private System.Windows.Forms.ColumnHeader colHeadName;
+        private System.Windows.Forms.ColumnHeader colHeadModel;
+        private System.Windows.Forms.ColumnHeader colHeadProblem;
+        private System.Windows.Forms.ColumnHeader colHeadTag;
+        private System.Windows.Forms.ColumnHeader colHeadCost;
+        private System.Windows.Forms.ColumnHeader colHeadName1;
+        private System.Windows.Forms.ColumnHeader colHeadModel1;
+        private System.Windows.Forms.ColumnHeader colHeadProblem1;
+        private System.Windows.Forms.ColumnHeader colHeadTag1;
+        private System.Windows.Forms.ColumnHeader colHeadCost1;
     }
 }
 
--- ServiceApplication-FormsApp/Form1.cs
+++ ServiceApplication-FormsApp/Form1.cs
@@ -15,6 +15,7 @@ namespace ServiceApplication_FormsApp
         public ServiceApplicationForm()
         {
             InitializeComponent();
+            InitialiseNumTag();
         }
         //6.2	Create a global List<T> of type Drone called “FinishedList”. 
         List<Drone> FinishedList = new List<Drone>();
@@ -24,6 +25,14 @@ namespace ServiceApplication_FormsApp
 
         //6.4	Create a global Queue<T> of type Drone called “ExpressService”.
         Queue<Drone> ExpressService = new Queue<Drone>();
+
+        private void InitialiseNumTag()
+        {
+            numTag.Maximum = 900;
+            numTag.Minimum = 100;
+            numTag.Value = numTag.Minimum;
+            numTag.Increment = 10;
+        }
 
         #region methods
         //6.5	Create a button method called “AddNewItem” that will add a new service item to a Queue<> based on the priority.
@@ -37,10 +46,10 @@ namespace ServiceApplication_FormsApp
                 string model = tbModel.Text;
                 string problem = tbProblem.Text;
                 double cost = double.Parse(tbCost.Text);
-                int tag = IncrementTag(int.Parse(numTag.Text));
+                int tag = int.Parse(numTag.Text);
                 int priority = GetServicePriority();
 
-                if (priority == 1)
+                if (priority == 0)
                 {
                     Drone newRegular = new Drone();
                     newRegular.setName(name);
@@ -49,11 +58,11 @@ namespace ServiceApplication_FormsApp
                     newRegular.setCost(cost);
                     newRegular.setTag(tag);
                     RegularService.Enqueue(newRegular);
+
+                    DisplayRegular();
                 }
 
-                DisplayRegular();
-
-                if (priority == 0)
+                if (priority == 1)
                 {
                     Drone newExpress = new Drone();
                     newExpress.setName(name);
@@ -62,8 +71,9 @@ namespace ServiceApplication_FormsApp
                     newExpress.setCost(ExpressCost(cost));
                     newExpress.setTag(tag);
                     ExpressService.Enqueue(newExpress);
+
+                    DisplayExpress();
                 }
-                DisplayExpress();
                 ClearTextboxes();
             }
             catch (Exception ex)
@@ -96,6 +106,10 @@ namespace ServiceApplication_FormsApp
         private void DisplayRegular()
         {
             lvRegular.Items.Clear();
+            foreach (var item in RegularService)
+            {
+                lvRegular.Items.Add(new ListViewItem(new string[] { item.getName(), item.getModel(), item.getProblem(), item.getCost().ToString(), item.getTag().ToString() }));
+            }
         }
 
         //6.9	Create a custom method that will display all the elements in the ExpressService queue.
@@ -128,140 +142,153 @@ namespace ServiceApplication_FormsApp
             {
                 newTag = 100;
             }
+            numTag.Text = newTag.ToString();
             return newTag;
         }
 
         #endregion
 
         //6.12	Create a mouse click method for the regular service ListView that will display the Client Name and Service Problem in the related textboxes.
-        private void lvRegular_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void lvRegular_MouseClick(object sender, MouseEventArgs e)
         {
             try
             {
-                if (lvRegular.SelectedItems.Count > 0)
-                {
-                    var item = lvRegular.SelectedItems[0] as Drone;
-                    if (item != null)
-                    {
-                        tbClientName.Text = item.getName();
-                        tbDroneModel.Text = item.getModel();
-                        tbServiceProblem.Text = item.getProblem();
-                        iuoServiceTag.Text = item.getTag().ToString();
-                        rbRegular.IsChecked = true;
-                        tbServiceCost.Text = item.getCost().ToString();
-                    }
-                }
+                int index = lvRegular.SelectedIndices[0];
+                Drone item = RegularService.ElementAt(index);
+                tbName.Text = item.getName();
+                tbModel.Text = item.getModel();
+                tbProblem.Text = item.getProblem();
+                numTag.Text = item.getTag().ToString();
+                rbRegular.Checked = true;
+                tbCost.Text = item.getCost().ToString();
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error when selecting data from the regular listview. {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show($"Error when selecting data from the listview. {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-
         //6.13	Create a mouse click method for the express service ListView that will display the Client Name and Service Problem in the related textboxes.
-        private void lvExpress_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void lvExpress_MouseClick(object sender, MouseEventArgs e)
         {
             try
             {
-                if (lvExpress.SelectedItems.Count > 0)
-                {
-                    var item = lvExpress.SelectedItems[0] as Drone;
-                    if (item != null)
-                    {
-                        tbClientName.Text = item.getName();
-                        tbDroneModel.Text = item.getModel();
-                        tbServiceProblem.Text = item.getProblem();
-                        iuoServiceTag.Text = item.getTag().ToString();
-                        rbRegular.IsChecked = true;
-                        tbServiceCost.Text = item.getCost().ToString();
-                    }
-                }
+                int index = lvExpress.SelectedIndices[0];
+                Drone item = ExpressService.ElementAt(index);
+                tbName.Text = item.getName();
+                tbModel.Text = item.getModel();
+                tbProblem.Text = item.getProblem();
+                numTag.Text = item.getTag().ToString();
+                rbRegular.Checked = true;
+                tbCost.Text = item.getCost().ToString();
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error when selecting data from the express listview. {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show($"Error when selecting data from the listview. {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         //6.14	Create a button click method that will remove a service item from the regular ListView and dequeue the regular service Queue<T> data structure.
         //The dequeued item must be added to the List<T> and displayed in the ListBox for finished service items.
-        private void btnRegular_Click(object sender, RoutedEventArgs e)
+        private void btnRegular_Click(object sender, EventArgs e)
         {
             try
             {
-                if (lvRegular.SelectedItems.Count > 0)
-                {
-                    Drone item = lvRegular.SelectedItems[0] as Drone;
-                    lvRegular.Items.Remove(item);
-                    RegularService.Dequeue();
-                    FinishedList.Add(item);
-                    lbFinished.ItemsSource = null;
-                    lbFinished.ItemsSource = FinishedList;
-                }
+                Drone queue = RegularService.ElementAt(0);
+                string name = queue.getName();
+                double cost = queue.getCost();
+
+                RegularService.Dequeue();
+                lvRegular.Items.RemoveAt(0);
+
+                Drone item = new Drone();
+                item.setName(name);
+                item.setCost(cost);
+                FinishedList.Add(item);
+
+                lbFinished.DataSource = FinishedList;
+                lbFinished.DisplayMember = $"{name} - ${cost}";
+                lbFinished.ValueMember = null;
+                lbFinished.Refresh();
+
+                //lbFinished.Items.Clear();
+                //foreach (var finished in FinishedList)
+                //{
+                //    lbFinished.Items.Add(new ListViewItem(new[] { finished.getName(), finished.getCost().ToString() }));
+                //}
+
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error when completing selected entry from the regular listview. {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show($"Error when completing selected entry from the regular listview. {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
 
         //6.15	Create a button click method that will remove a service item from the express ListView and dequeue the express service Queue<T> data structure.
         //The dequeued item must be added to the List<T> and displayed in the ListBox for finished service items.
-        private void btnExpress_Click(object sender, RoutedEventArgs e)
+        private void btnExpress_Click(object sender, EventArgs e)
         {
             try
             {
-                if (lvExpress.SelectedItems.Count > 0)
-                {
-                    Drone item = lvExpress.SelectedItems[0] as Drone;
-                    lvRegular.Items.Remove(item);
-                    ExpressService.Dequeue();
-                    FinishedList.Add(item);
-                    lbFinished.ItemsSource = null;
-                    lbFinished.ItemsSource = FinishedList;
-                }
+                Drone queue = ExpressService.ElementAt(0);
+                string name = queue.getName();
+                double cost = queue.getCost();
+
+                ExpressService.Dequeue();
+                lvExpress.Items.RemoveAt(0);
+
+                Drone item = new Drone();
+                item.setName(name);
+                item.setCost(cost);
+                FinishedList.Add(item);
+
+                lbFinished.DataSource = FinishedList;
+                lbFinished.DisplayMember = name;
+                lbFinished.DisplayMember = cost.ToString();
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error when completing selected entry from the express listview. {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show($"Error when completing selected entry from the regular listview. {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        //6.16	Create a double mouse click method that will delete a service item from the finished listbox and remove the same item from the List<T>.
-        private void lbFinished_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            try
-            {
-                if (lbFinished.SelectedItem != null)
-                {
-                    Drone selectedItem = (Drone)lbFinished.SelectedItem;
-                    FinishedList.Remove(selectedItem);
-                    lbFinished.Items.Remove(selectedItem);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error when deleting selected entry from the finished listbox. {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
-            }
-        }
+        ////6.16	Create a double mouse click method that will delete a service item from the finished listbox and remove the same item from the List<T>.
+        //private void lbFinished_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        //{
+        //    try
+        //    {
+        //        if (lbFinished.SelectedItem != null)
+        //        {
+        //            Drone selectedItem = (Drone)lbFinished.SelectedItem;
+        //            FinishedList.Remove(selectedItem);
+        //            lbFinished.Items.Remove(selectedItem);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Error when deleting selected entry from the finished listbox. {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        //    }
+        //}
         //6.17	Create a custom method that will clear all the textboxes after each service item has been added.
         private void ClearTextboxes()
         {
-            tbClientName.Text = string.Empty;
-            tbDroneModel.Text = string.Empty;
-            tbServiceProblem.Text = string.Empty;
-            iuoServiceTag.Value = 100;
-            tbServiceCost.Text = string.Empty;
+            tbName.Text = string.Empty;
+            tbModel.Text = string.Empty;
+            tbProblem.Text = string.Empty;
+            numTag.Value = 100;
+            tbCost.Text = string.Empty;
 
         }
 
         #region Buttons
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e)
         {
             AddNewItem();
         }
+
 
         #endregion
         //6.18	All code is required to be adequately commented. Map the programming criteria and features to your code/methods
